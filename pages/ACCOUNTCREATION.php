4bbd85abaa29098ba72528718c0adfe64db45eb2
jasonvanmalder <jasonvanmalder@gmail.com>
2015-08-08 20:43:47 +0200

    Installation improvements

---
+++ pages/ACCOUNTCREATION.php
@@ -0,0 +1,19 @@
+<?php
+	if(isset($_POST['Rsubmit'])){
+		if(isset($_POST['username']) && isset($_POST['password']) && isset($_POST['email']) && $_POST['username'] != '' && $_POST['password'] != '' && $_POST['email'] != ''){
+			// register process
+			echo $user->register($_POST['username'],$_POST['email'],$_POST['password']);
+        }else
+		{
+			$app::redirect("index.php?page=welcome&register=failed");
+		}
+	}
+?>
+<form id="register" name="register" method="POST">
+<?php
+	echo $form->input("username","Nom d'utilisateur","text");
+	echo $form->input("email","Adresse email","email");
+    echo $form->input("password","Mot de passe","password");
+?>
+<button type="submit" name="Rsubmit" id="submit" class="btn btn-primary">S'inscrire</button>
+</form>
